2932ee63d9111ba7335d2bf14354ce5e1c2f4d32
hadysoufan <hadisoufan74@gmail.com>
2022-11-10 19:51:36 +0200

    adding student info page

--- script.js
+++ script.js
@@ -12,5 +12,13 @@ labels.forEach((label) => {
     .join("");
 });
 
+// //////////////////////////
+// Search script
 
+const inputBox = document.querySelector(".input-box"),
+   searchIcon = document.querySelector(".icon"),
+   closeIcon = document.querySelector(".close-icon");
+
+ searchIcon.addEventListener("click", () => inputBox.classList.add("open"));
+ closeIcon.addEventListener("click", () => inputBox.classList.remove("open"));
 
